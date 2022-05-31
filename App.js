b1e6f60d689b65ae4aae953716009ad00b0baf41
reggyzian <reggyzian.purnama@neuronworks.co.id>
2022-05-31 18:48:26 +0700

    Move app js

---
+++ App.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use('/', require('./src/routes'));
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`);
+});
