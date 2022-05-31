6c9d4ad5aaae36b9e7728b2b142609e37530229d
reggyzian <reggyzian.purnama@neuronworks.co.id>
2022-05-31 19:10:02 +0700

    Add hello world to web

--- src/routes/Index.js
+++ src/routes/Index.js
@@ -2,6 +2,16 @@ const router = require('express').Router();
 const Guest = require('./GuestRouter');
 const Message = require('./MessageRouter');
 
+const Main = {
+    home(req, res){
+        res.send({ 
+            success: true, 
+            message: 'Hello World'
+        });
+    }
+}
+
+router.get('/', Main.home);
 router.use('/guest', Guest);
 router.use('/message', Message);
 
