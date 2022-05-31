566ef35b2694eb9548c8da0c5cde71e248bb5080
reggyzian <reggyzian.purnama@neuronworks.co.id>
2022-05-31 16:32:15 +0700

    Add port production

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
+const PORT = process.env.PORT || 8000;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use('/', require('./routes'));
 
-app.listen(8000, ()=>{
-    console.log('Server Berjalan di Port : 8000');
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`);
 });
