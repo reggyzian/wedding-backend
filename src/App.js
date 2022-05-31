ebde3ee6004947f814a58cff944febc58e8af094
reggyzian <reggyzian.purnama@neuronworks.co.id>
2022-05-31 18:34:54 +0700

    Change port

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }))
