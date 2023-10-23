9d4e307ce1bc66125930479fff1d009873a953b9
Reggy Zian Purnama <reggyzianpurnama@gmail.com>
2023-10-23 20:38:10 +0700

    feat: removing auth

--- src/configs/Database.js
+++ src/configs/Database.js
@@ -1,9 +1,9 @@
 module.exports = {
-    user : 'bwuofmhwizuxyh',
+    user : process.env.user_name,
     host : 'ec2-34-230-153-41.compute-1.amazonaws.com',
-    database : 'd6pnc6tsab0c8f',
-    password : '7b1aacb2998ed0cf0da8d75a2736d92a6a842d74bfcc7d5908eecb8cd88b0d14',
-    port : 5432,
+    database : process.env.database,
+    password : process.env.password,
+    port : process.env.port,
     ssl: {
         rejectUnauthorized: false,
     },
